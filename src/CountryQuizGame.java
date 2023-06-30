04b659305203377431789a40da8d9e44f9cb54ca
CambellT <115440007+CambellT@users.noreply.github.com>
2023-06-30 14:43:56 +1200

    Adjusted countries.csv
    Adjusted to only have standard characters

--- src/CountryQuizGame.java
+++ src/CountryQuizGame.java
@@ -9,9 +9,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.*;
+import java.text.Normalizer;
+import java.util.regex.Pattern;
 
 public class CountryQuizGame { 
     private static final String DELIMITER = ",";
+    private static final String SEPARATOR = "------------------------------------------"; 
 
     /**  
      * Loads the countries from the csv file into the set of all countries
@@ -23,7 +26,7 @@ public class CountryQuizGame {
             String filePath = getFilePath(fileName);
             List<String> lines = Files.readAllLines(Path.of(filePath));
             for (String line : lines) {
-                String[] values = line.split(",");
+                String[] values = line.split(DELIMITER);
                 String name = values[0].replace("\"", "");
                 String capital = values[1].replace("\"", "");
                 Country c = new Country(name, capital);
@@ -56,19 +59,26 @@ public class CountryQuizGame {
         List<Country> allCountries = loadCountries();
         Scanner scanner = new Scanner(System.in);
         int correctCount = 0;
-        int numRounds = 3;
 
         System.out.println("==========================================");
         System.out.println("Welcome to the Country Capitals Quiz!");
-        System.out.println("Enter the Capital City of each country, or type 'quit' to exit.");
+        System.out.println("Enter the Capital City of each Country,");
+        System.out.println("or type 'quit' to exit.");
+        System.out.println(SEPARATOR); 
+        System.out.println("How many rounds would you like to play?");
 
-        for (int i = 0; i < numRounds; i++) {
+        int numRounds = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 1; i < numRounds + 1; i++) {
             Random random = new Random();
             int randomIndex = random.nextInt(allCountries.size());
             Country currentCountry = allCountries.get(randomIndex);
-            System.out.println("------------------------------------------"); 
+            String currentCity = currentCountry.capital();
+            System.out.println(SEPARATOR);
+            System.out.println("Round "+ i); 
             System.out.println("Enter the Capital City for "+ currentCountry.name() +": ");
-            System.out.println("------------------------------------------");
+            System.out.println(SEPARATOR); 
 
             String input = scanner.nextLine();
 
@@ -78,20 +88,24 @@ public class CountryQuizGame {
                 break;
             }
 
-            if (input.equalsIgnoreCase(currentCountry.capital())) { 
-                System.out.println("------------------------------------------");
-                System.out.println("Well done, that is Correct!");
+            if (preprocessString(input).equalsIgnoreCase(currentCity)) { 
+                System.out.println(SEPARATOR); 
+                System.out.println("Well done, "+ currentCity +" is Correct!");
+                System.out.println(input);
                 correctCount++;
             }
             else{
-                System.out.println("------------------------------------------");
+                System.out.println(SEPARATOR); 
                 System.out.println("Incorrect, the answer was "+currentCountry.capital()+".");
             }
         }
 
-        System.out.println("------------------------------------------");
+        System.out.println(SEPARATOR); 
         if (correctCount == numRounds) {
             System.out.println("Well done! You got every question correct!");
+        }
+        else if (correctCount > 1) {
+            System.out.println("You got "+correctCount+" questions correct out of "+numRounds+".");
         }
         else if (correctCount > 0) {
             System.out.println("You got "+correctCount+" question correct out of "+numRounds+".");
@@ -99,8 +113,18 @@ public class CountryQuizGame {
         else {
             System.out.println("Sorry, you got no questions correct.");
         }
-        System.out.println("------------------------------------------");
+        System.out.println(SEPARATOR); 
 
+    }
+
+    public static String preprocessString(String s) {
+        // Remove punctuation
+        String noPunctuation = s.replaceAll("\\p{Punct}", "");
+
+        // Remove spaces
+        String noSpaces = noPunctuation.replaceAll("\\s", "");
+
+        return noSpaces.toLowerCase();
     }
 
 }
