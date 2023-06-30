5e9f0d6a0fd8886d0193cf3579d82a9dfaf264d6
CambellT <115440007+CambellT@users.noreply.github.com>
2023-06-30 15:44:38 +1200

    Added country guessing part
    Separated the methods for tidiness and added ability to guess a country from a given capital city

--- src/CountryQuizGame.java
+++ src/CountryQuizGame.java
@@ -52,23 +52,70 @@ public class CountryQuizGame {
         return null;
     }
 
+    // Process the string to ignore spaces and punctuation
+    public static String preprocessString(String s) {
+        // Remove punctuation
+        String noPunctuation = s.replaceAll("\\p{Punct}", "");
+        // Remove spaces
+        String noSpaces = noPunctuation.replaceAll("\\s", "");
+        return noSpaces.toLowerCase();
+    }
+
     public static void main(String[] args) {
         String fileName = "countries.csv";
         String filePath = getFilePath(fileName);
 
         List<Country> allCountries = loadCountries();
         Scanner scanner = new Scanner(System.in);
-        int correctCount = 0;
+        int correctCount;
 
         System.out.println("==========================================");
         System.out.println("Welcome to the Country Capitals Quiz!");
-        System.out.println("Enter the Capital City of each Country,");
-        System.out.println("or type 'quit' to exit.");
-        System.out.println(SEPARATOR); 
         System.out.println("How many rounds would you like to play?");
-
         int numRounds = scanner.nextInt();
         scanner.nextLine();
+        System.out.println(SEPARATOR);
+
+        System.out.println("Would you like to guess Countries(A) or Capitals(B)?");
+        String gameType = scanner.next();
+        System.out.println(gameType);
+        scanner.nextLine();
+        
+        if (gameType.equalsIgnoreCase("A")||gameType.equalsIgnoreCase("Countries")) {
+            correctCount = playCountryGame(numRounds, allCountries);
+        }
+        else if (gameType.equalsIgnoreCase("A")||gameType.equalsIgnoreCase("Countries")) {
+            correctCount = playCapitalGame(numRounds, allCountries);
+        }
+        else {
+            System.out.println("Input not recognised");
+            return;
+        }
+
+        // Tells the user how many questions they got correct
+        System.out.println(SEPARATOR); 
+        if (correctCount == numRounds) {
+            System.out.println("Well done! You got every question correct!");
+        }
+        else if (correctCount > 1) {
+            System.out.println("You got "+correctCount+" questions correct out of "+numRounds+".");
+        }
+        else if (correctCount > 0) {
+            System.out.println("You got "+correctCount+" question correct out of "+numRounds+".");
+        }
+        else {
+            System.out.println("Sorry, you got no questions correct.");
+        }
+        System.out.println(SEPARATOR); 
+
+    }
+
+    public static int playCapitalGame(int numRounds, List<Country> allCountries){
+        Scanner scanner = new Scanner(System.in);
+        int correctCount = 0;
+        System.out.println("Enter the Capital City of each Country,");
+        System.out.println("or type 'quit' to exit.");
+        System.out.println(SEPARATOR);  
 
         for (int i = 1; i < numRounds + 1; i++) {
             Random random = new Random();
@@ -88,7 +135,7 @@ public class CountryQuizGame {
                 break;
             }
 
-            if (preprocessString(input).equalsIgnoreCase(currentCity)) { 
+            if (preprocessString(input).equalsIgnoreCase(preprocessString(currentCity))) { 
                 System.out.println(SEPARATOR); 
                 System.out.println("Well done, "+ currentCity +" is Correct!");
                 System.out.println(input);
@@ -96,35 +143,49 @@ public class CountryQuizGame {
             }
             else{
                 System.out.println(SEPARATOR); 
-                System.out.println("Incorrect, the answer was "+currentCountry.capital()+".");
+                System.out.println("Incorrect, the answer was "+currentCity+".");
             }
         }
-
-        System.out.println(SEPARATOR); 
-        if (correctCount == numRounds) {
-            System.out.println("Well done! You got every question correct!");
-        }
-        else if (correctCount > 1) {
-            System.out.println("You got "+correctCount+" questions correct out of "+numRounds+".");
-        }
-        else if (correctCount > 0) {
-            System.out.println("You got "+correctCount+" question correct out of "+numRounds+".");
-        }
-        else {
-            System.out.println("Sorry, you got no questions correct.");
-        }
-        System.out.println(SEPARATOR); 
-
+        return correctCount;
     }
 
-    public static String preprocessString(String s) {
-        // Remove punctuation
-        String noPunctuation = s.replaceAll("\\p{Punct}", "");
+    public static int playCountryGame(int numRounds, List<Country> allCountries){
+        Scanner scanner = new Scanner(System.in);
+        int correctCount = 0;
+        System.out.println("Enter the Country that corresponds to the");
+        System.out.println("Capital City, or type 'quit' to exit.");
+        System.out.println(SEPARATOR);  
 
-        // Remove spaces
-        String noSpaces = noPunctuation.replaceAll("\\s", "");
+        for (int i = 1; i < numRounds + 1; i++) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(allCountries.size());
+            Country currentCountry = allCountries.get(randomIndex);
+            String countryName = currentCountry.name();
+            String currentCity = currentCountry.capital();
+            System.out.println(SEPARATOR);
+            System.out.println("Round "+ i); 
+            System.out.println("Enter the Country that "+ currentCity +" is the Capital of: ");
+            System.out.println(SEPARATOR); 
 
-        return noSpaces.toLowerCase();
+            String input = scanner.nextLine();
+
+            // Type quit 
+            if (input.equalsIgnoreCase("quit")) {
+                System.out.println("Goodbye!");
+                break;
+            }
+
+            if (preprocessString(input).equalsIgnoreCase(preprocessString(countryName))) { 
+                System.out.println(SEPARATOR); 
+                System.out.println("Well done, "+ countryName +" is Correct!");
+                System.out.println(input);
+                correctCount++;
+            }
+            else{
+                System.out.println(SEPARATOR); 
+                System.out.println("Incorrect, the answer was "+countryName+".");
+            }
+        }
+        return correctCount;
     }
-
 }
