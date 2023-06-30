7e861a5e9a2361d5147407eecff062e8f9894976
CambellT <115440007+CambellT@users.noreply.github.com>
2023-06-30 13:42:19 +1200

    Improved the questions being asked

--- src/CountryQuizGame.java
+++ src/CountryQuizGame.java
@@ -16,26 +16,25 @@ public class CountryQuizGame {
     /**  
      * Loads the countries from the csv file into the set of all countries
      */
-    public static Set<Country> loadCountries(){
-        Set<Country> allCountries = new HashSet<Country>(); 
+    public static List<Country> loadCountries(){
+        List<Country> allCountries = new ArrayList<Country>(); 
         try {
             String fileName = "countries.csv";
             String filePath = getFilePath(fileName);
             List<String> lines = Files.readAllLines(Path.of(filePath));
             for (String line : lines) {
-                //System.out.println(line);
                 String[] values = line.split(",");
                 String name = values[0].replace("\"", "");
                 String capital = values[1].replace("\"", "");
                 Country c = new Country(name, capital);
                 allCountries.add(c);
             }
-            System.out.printf("Loaded %d countries.", allCountries.size());
+            System.out.printf("Loaded %d countries.%n", allCountries.size());
         } catch(IOException e){
             System.out.println("File reading failed");
         }
         for (Country c : allCountries){
-            System.out.println(c.toString());
+            //System.out.println(c.toString());
         }
         return allCountries;
     }
@@ -54,21 +53,54 @@ public class CountryQuizGame {
         String fileName = "countries.csv";
         String filePath = getFilePath(fileName);
 
-        if (filePath != null) {
-            System.out.println("Good file");
-        } else {
-            System.out.println("File is not accessible.");
-        }
-        Set<Country> allCountries = loadCountries();
+        List<Country> allCountries = loadCountries();
+        Scanner scanner = new Scanner(System.in);
+        int correctCount = 0;
+        int numRounds = 3;
 
+        System.out.println("==========================================");
         System.out.println("Welcome to the Country Capitals Quiz!");
+        System.out.println("Enter the Capital City of each country, or type 'quit' to exit.");
 
-        while (true) {
-            System.out.println("==========================================");
-            System.out.println("Enter a country name (or 'quit' to exit): ");
+        for (int i = 0; i < numRounds; i++) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(allCountries.size());
+            Country currentCountry = allCountries.get(randomIndex);
+            System.out.println("------------------------------------------"); 
+            System.out.println("Enter the Capital City for "+ currentCountry.name() +": ");
             System.out.println("------------------------------------------");
-            break;
+
+            String input = scanner.nextLine();
+
+            // Type quit 
+            if (input.equalsIgnoreCase("quit")) {
+                System.out.println("Goodbye!");
+                break;
+            }
+
+            if (input.equalsIgnoreCase(currentCountry.capital())) { 
+                System.out.println("------------------------------------------");
+                System.out.println("Well done, that is Correct!");
+                correctCount++;
+            }
+            else{
+                System.out.println("------------------------------------------");
+                System.out.println("Incorrect, the answer was "+currentCountry.capital()+".");
+            }
         }
+
+        System.out.println("------------------------------------------");
+        if (correctCount == numRounds) {
+            System.out.println("Well done! You got every question correct!");
+        }
+        else if (correctCount > 0) {
+            System.out.println("You got "+correctCount+" question correct out of "+numRounds+".");
+        }
+        else {
+            System.out.println("Sorry, you got no questions correct.");
+        }
+        System.out.println("------------------------------------------");
+
     }
 
 }
